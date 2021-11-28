4c55f434bc23f40f0543e3472d326ddb84a1702b
Thomas Quinton <cartmam1@hotmail.fr>
2021-11-28 14:54:36 +0100

    Update server.php

--- server.php
+++ server.php
@@ -1,62 +1,8 @@
 <?php
-<<<<<<< HEAD
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Content-type: text/html; charset=UTF-8'); 
-header('Access-Control-Allow-Headers: X-Requested-With');
-/**
- * 
- */
-class Game_Server 
-{
-	private $bdd , $game ,$chat,$player,$hidenword,$round,$link_game;
-	function __construct()
-	{
-		 $this->bdd= new pdo('mysql:host=mysql-m2x.alwaysdata.net;dbname=m2x_game','m2x','moul_976',array(PDO::ATTR_ERRMODE=> PDO::ERRMODE_EXCEPTION)) ;
-		 $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	/**
-	 * 
-	 *On vas  cree une partie dans notre bdd 
-	 */
-	public function Set_Game($round_number,$max_player,$player_activ,$link_game) : void 
-	{
-		$setmessage=$this->bdd->prepare('INSERT INTO game(round_number,max_player,player_activ,link_game) VALUES (?,?,?,?)');
-		$setmessage->execute(array($round_number,$max_player,$player_activ,$link_game));
-		$this->game=$this->getgame();
-	}
-	public function Edit_Game($round_number,$max_player,$player_activ) : void 
-	{
-		$setmessage=$this->bdd->prepare('UPDATE game SET round_number =? ,max_player = ?,player_activ = ?  WHERE id_game = ?');
-		$setmessage->execute(array($round_number,$max_player,$player_activ,$this->getgame()));
-	}
-	public function getgame() :string
-	{
-		$setgame=$this->bdd->query('SELECT * from game order by id_game desc limit 1');
-		$value=$setgame->fetch();
-		return $value['id_game'];
-	}
-	public function getchat() :string
-	{
-		$setgame=$this->bdd->query('SELECT * from chat order by id_chat desc limit 1');
-		$value=$setgame->fetch();
-		return $value['id_chat'];
-	}
-	/**
-	 * 
-	 *On vas ecrire un message dans notre bdd 
-	 */
-	public function Setmessage($text) :void
-	{
-		$setmessage=$this->bdd->prepare('INSERT INTO message(text,id_game,id_player) VALUES (?,?,?)');
-		$setmessage->execute(array($text,$this->getgame(),$this->getplayer()));
-	}
-=======
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 	header("Content-type: text/html; charset=UTF-8"); 
 	header("Access-Control-Allow-Headers: X-Requested-With");
->>>>>>> fb056af7cbf135c40e783a07ffd86df4fe2f870e
 
 	class Game_Server {
 		private $bdd, $game, $chat, $player, $hiddenword;
