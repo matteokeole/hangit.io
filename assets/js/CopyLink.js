9c3187ece183f437ed3dce001d7f55f155e10a5d
Mattéo Legagneux <matteoo34@icloud.com>
2021-11-29 20:03:15 +0100

    Ajax addEventListener()

--- assets/js/CopyLink.js
+++ assets/js/CopyLink.js
@@ -7,33 +7,29 @@ Button.copyLink.addEventListener("click", () => {
 	document.execCommand("copy")
 });
 
-
-let req = new XMLHttpRequest(),
-	url = `https://m2x.alwaysdata.net/hangit/server.php?liens=${current_url}`,
-	data = "";
-req.open("GET", url);
-req.send();
-req.onreadystatechange = function() {
-	if (req.status == 200) {
-		data = JSON.parse(req.response);
-		if (data.liens) {
-			// Join game
-			toggleDisplay(Container.nickname);
-			toggleDisplay(Container.joinGame);
-			console.info("Joining game")
-		} else if (current_url.includes("?")) {
-			// There is a link but it is invalid
-			GameTip.textContent = Return.tip.invalidLink;
-			toggleDisplay(GameTip)
-			console.log('test');
-		} else if (!data.liens) {
-			// Host game
-			toggleDisplay(Container.nickname);
-			toggleDisplay(Container.openHostForm);
-			GameTip.textContent = Return.tip.joinGame;
-			toggleDisplay(GameTip);
-			sendData("Link_game", Input.invitationLink.value)
-		}
-	}
-}
+let r = new XMLHttpRequest(),
+    url = `https://m2x.alwaysdata.net/hangit/server.php?liens=${current_url}`,
+    data = "";
+r.open("GET", url);
+r.send();
+r.addEventListener("load", () => {
+    data = JSON.parse(r.response);
+    if (data.liens) {
+        // Join game
+        toggleDisplay(Container.nickname);
+        toggleDisplay(Container.joinGame);
+        console.info("Joining game")
+    } else if (current_url.includes("?")) {
+        // There is a link but it is invalid
+        GameTip.textContent = Return.tip.invalidLink;
+        toggleDisplay(GameTip)
+    } else if (!data.liens) {
+        // Host game
+        toggleDisplay(Container.nickname);
+        toggleDisplay(Container.openHostForm);
+        GameTip.textContent = Return.tip.joinGame;
+        toggleDisplay(GameTip);
+        sendData("Link_game", Input.invitationLink.value)
+    }
+})
 // console.log(test('https://luha.alwaysdata.net/api/'))
--- assets/js/GenerateLink.js
+++ assets/js/GenerateLink.js
@@ -7,4 +7,4 @@ const GenerateLink = () => {return (new Date()).getTime()};
 // Generate game link (local test)
 // Input.invitationLink.value = `http://localhost/hangit.io/?g=${GenerateLink()}`
 // Generate game link (local test 2)
- Input.invitationLink.value = `http://localhost:2021/?g=${GenerateLink()}`
+ Input.invitationLink.value = `http://localhost/hangit.io/?g=${GenerateLink()}`
