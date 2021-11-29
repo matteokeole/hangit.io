33752166be2fe4a47f1ef5aae6f1c4065ca9932e
Mattéo Legagneux <matteoo34@icloud.com>
2021-11-29 17:05:25 +0100

    Update JoinGame.js

--- assets/js/JoinGame.js
+++ assets/js/JoinGame.js
@@ -2,5 +2,9 @@ Button.joinGame.addEventListener("click", () => {
 	// Set player nickname
 	SetNickname(Input.nickname.value);
 	Input.nickname.value = "";
-	Input.nickname.disabled = true
+	Input.nickname.disabled = true;
+	// Toggle containers display
+	toggleDisplay(Container.nickname, "none");
+	toggleDisplay(Container.joinGame, "none");
+	toggleDisplay(Container.gameContainer, "flex")
 })
