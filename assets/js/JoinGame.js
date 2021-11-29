ef6ae963f2db68ffecc4b11a8870ce5f5a31324e
Mattéo Legagneux <matteoo34@icloud.com>
2021-11-29 22:32:33 +0100

    Update JoinGame.js

--- assets/js/JoinGame.js
+++ assets/js/JoinGame.js
@@ -2,7 +2,7 @@ Button.joinGame.addEventListener("click", () => {
 	// Set player nickname
 	SetNickname(Input.nickname.value);
 	// Send link to database
-	sendDataTwo("invite", Input.nickname.value, "joinlink", current_url);
+	// sendDataTwo("invite", Input.nickname.value, "joinlink", current_url);
 	Input.nickname.value = "";
 	Input.nickname.disabled = true;
 	// Join game
