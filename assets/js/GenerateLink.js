572ab8e867e6d98f838e4a7d7dcaa14f2d261dcb
Thomas Quinton <cartmam1@hotmail.fr>
2021-11-29 21:33:25 +0100

    Update GenerateLink.js

--- assets/js/GenerateLink.js
+++ assets/js/GenerateLink.js
@@ -2,9 +2,9 @@
 const GenerateLink = () => {return (new Date()).getTime()};
 
 // Generate game link
-//Input.invitationLink.value = `https://matteoo34.github.io/hangit.io/?g=${GenerateLink()}`;
+Input.invitationLink.value = `https://matteoo34.github.io/hangit.io/?g=${GenerateLink()}`;
 
 // Generate game link (local test)
 // Input.invitationLink.value = `http://localhost/hangit.io/?g=${GenerateLink()}`
 // Generate game link (local test 2)
- Input.invitationLink.value = `http://localhost:2021/?g=${GenerateLink()}`
+//  Input.invitationLink.value = `http://localhost:2021/?g=${GenerateLink()}`
