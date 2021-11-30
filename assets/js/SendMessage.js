be22a7d2c9fb927609c225162d3cd14182072b57
Mattéo Legagneux <matteoo34@icloud.com>
2021-11-30 22:18:38 +0100

    Messages dates

--- assets/js/SendMessage.js
+++ assets/js/SendMessage.js
@@ -6,122 +6,146 @@ Input.message.addEventListener("input", () => {
 
 // Send message function
 const checkMessage = (msg) => {
-	// Check if a sent message matches with the hidden word
-	msg = msg.toUpperCase();
-	if (msg[0] == "!") {
-		// Command
-		msg = msg.substr(1, msg.length - 1);
-		if (/^[A-ZÀ-Ú]{1}$/.test(msg)) {
-			// Letter sent, reveal it on the hidden word if valid
-			// Test if the letter hasn't already been proposed
-			let letterAlreadyProposed = false;
-			for (let i = 0; i < HiddenWord.sentLetters.length; i++) {
-				if (HiddenWord.sentLetters[i] == msg) letterAlreadyProposed = true
-			}
-			if (letterAlreadyProposed) sendMessage(true, "Vous avez déjà proposé cette lettre !");
-			else {
-				// Add letter to submitted letters
-				HiddenWord.sentLetters.push(msg);
-				// Check for letter in hidden word
-				HiddenWord.currentInputValidity = checkForCharInWord(msg);
-				if (HiddenWord.originalWord == HiddenWord.displayWord) {
-					Player.score += 100;
-					nextRoundPlayer()
+		// Check if a sent message matches with the hidden word
+		msg = msg.toUpperCase();
+		if (msg[0] == "!") {
+			// Command
+			msg = msg.substr(1, msg.length - 1);
+			if (/^[A-ZÀ-Ú]{1}$/.test(msg)) {
+				// Letter sent, reveal it on the hidden word if valid
+				// Test if the letter hasn't already been proposed
+				let letterAlreadyProposed = false;
+				for (let i = 0; i < HiddenWord.sentLetters.length; i++) {
+					if (HiddenWord.sentLetters[i] == msg) letterAlreadyProposed = true
+				}
+				if (letterAlreadyProposed) sendMessage(true, "Vous avez déjà proposé cette lettre !");
+				else {
+					// Add letter to submitted letters
+					HiddenWord.sentLetters.push(msg);
+					// Check for letter in hidden word
+					HiddenWord.currentInputValidity = checkForCharInWord(msg);
+					if (HiddenWord.originalWord == HiddenWord.displayWord) {
+						Player.score += 100;
+						nextRoundPlayer()
+					}
+				}
+			} else if (msg.length > 1) {
+				// Word sent, reveal it on the hidden word if valid
+				// Test if the word hasn't already been proposed
+				let wordAlreadyProposed = false;
+				for (let i = 0; i < HiddenWord.sentWords.length; i++) {
+					if (HiddenWord.sentWords[i] == msg) wordAlreadyProposed = true
+				}
+				if (wordAlreadyProposed) sendMessage(true, "Vous avez déjà proposé ce mot !");
+				else {
+					// Add letter to submitted letters
+					HiddenWord.sentWords.push(msg);
+					// Check if the hidden word is found
+					HiddenWord.currentInputValidity = checkForFullWord(msg);
+					// If found, increment score & next round
+					if (HiddenWord.originalWord == HiddenWord.displayWord) {
+						Player.score += 200;
+						nextRoundPlayer()
+					}
 				}
 			}
-		} else if (msg.length > 1) {
-			// Word sent, reveal it on the hidden word if valid
-			// Test if the word hasn't already been proposed
-			let wordAlreadyProposed = false;
-			for (let i = 0; i < HiddenWord.sentWords.length; i++) {
-				if (HiddenWord.sentWords[i] == msg) wordAlreadyProposed = true
+		}
+		if (!HiddenWord.currentInputValidity) {
+			// Invalid input, +1 error
+			HiddenWord.invalidInputs++;
+			// Display remaining tries
+			let remainingTries = (11 - HiddenWord.invalidInputs),
+				s = (remainingTries > 1) ? "s" : "";
+			RemainingTries.textContent = (remainingTries > 0) ? `${remainingTries} essai${s} restant${s}.` : "Pendu(e) !";
+			if (HiddenWord.invalidInputs < 11) {
+				// Not enough errors to lose
+				toggleCanvasPart(HiddenWord.invalidInputs)
+			} else {
+				// Game over!
+				setTimeout(() => {Input.message.blur()});
+				toggleCanvasPart(11) // Show canvas last part
 			}
-			if (wordAlreadyProposed) sendMessage(true, "Vous avez déjà proposé ce mot !");
-			else {
-				// Add letter to submitted letters
-				HiddenWord.sentWords.push(msg);
-				// Check if the hidden word is found
-				HiddenWord.currentInputValidity = checkForFullWord(msg);
-				// If found, increment score & next round
-				if (HiddenWord.originalWord == HiddenWord.displayWord) {
-					Player.score += 200;
-					nextRoundPlayer()
+		}
+		// Set input validity to true
+		HiddenWord.currentInputValidity = true
+	},
+	sendMessage = (auto, msg, authorName, authorColor) => {
+		// Send a message on the chat
+		// auto = send automatic message (author-less, true|false)
+		// Create message DOM
+		let listFullHeight = MessageList.scrollHeight,
+			listVisibleHeight = MessageList.offsetHeight,
+			message = document.createElement("li"),
+			inner = document.createElement("div"),
+			content = document.createElement("span");
+		// Set element classes
+		message.className = "Message";
+		inner.className = "MessageInner";
+		content.className = "MessageContent";
+		// Set values
+		content.textContent = msg;
+		// Append elements
+		inner.appendChild(content);
+		// Non-automatic message
+		if (!auto) {
+			// User message, create author section
+			if (Chat.lastMessageSender == authorName) {
+				// This is the same author who sent the last message
+				// Do not display the author element and reduce message padding
+				message.style.padding = "0 4px"
+			} else {
+				// Message from a new author
+				// Display an author element
+				let author = document.createElement("span");
+				author.className = "MessageAuthor";
+				author.textContent = authorName;
+				author.style.color = authorColor;
+				message.appendChild(author)
+			}
+			// Update last message author
+			// console.log("Message envoyé par " + authorName)
+			Chat.lastMessageSender = authorName;
+			let date = document.createElement("span");
+			date.className = "MessageDate";
+			date.textContent = "maintenant";
+			inner.appendChild(date)
+		}
+		message.appendChild(inner);
+		// Show message
+		MessageList.appendChild(message);
+		refreshMessageDate(message);
+		setMessageListPosition(listFullHeight, listVisibleHeight)
+	},
+	setMessageListPosition = (fullHeight, visibleHeight) => {
+		// Scroll to end of the message list
+		if (MessageList.scrollTop == (fullHeight - visibleHeight)) {
+			// The user is already at the end of the message list, continue scrolling
+			MessageList.scrollTop = MessageList.scrollHeight
+		}
+	},
+	refreshMessageDate = (message) => {
+		let date = message.querySelector(".MessageDate"),
+			oldDate = Date.now(),
+			newDate,
+			delay = 0,
+			minutesInterval,
+			secondsInterval = setInterval(() => {
+				newDate = Date.now();
+				delay = Math.floor((newDate - oldDate) / 1000);
+				if (delay < 60) {
+					delay += "s";
+					date.textContent = delay
+				} else {
+					clearInterval(secondsInterval);
+					date.textContent = "1m";
+					minutesInterval = setInterval(() => {
+						newDate = Date.now();
+						delay = `${Math.floor(((newDate - oldDate) / 1000) / 60)}m`;
+						date.textContent = delay
+					}, 60000)
 				}
-			}
-		}
+			}, 10000)
 	}
-	if (!HiddenWord.currentInputValidity) {
-		// Invalid input, +1 error
-		HiddenWord.invalidInputs++;
-		// Display remaining tries
-		let remainingTries = (11 - HiddenWord.invalidInputs),
-			s = (remainingTries > 1) ? "s" : "";
-		RemainingTries.textContent = (remainingTries > 0) ? `${remainingTries} essai${s} restant${s}.` : "Pendu(e) !";
-		if (HiddenWord.invalidInputs < 11) {
-			// Not enough errors to lose
-			toggleCanvasPart(HiddenWord.invalidInputs)
-		} else {
-			// Game over!
-			setTimeout(() => {Input.message.blur()});
-			toggleCanvasPart(11) // Show canvas last part
-		}
-	}
-	// Set input validity to true
-	HiddenWord.currentInputValidity = true
-},
-sendMessage = (auto, msg, authorName, authorColor) => {
-	// Send a message on the chat
-	// auto = send automatic message (author-less, true|false)
-	// Create message DOM
-	let listFullHeight = MessageList.scrollHeight,
-		listVisibleHeight = MessageList.offsetHeight,
-		message = document.createElement("li"),
-		inner = document.createElement("div"),
-		content = document.createElement("span");
-	// Set element classes
-	message.className = "Message";
-	inner.className = "MessageInner";
-	content.className = "MessageContent";
-	// Set values
-	content.textContent = msg;
-	// Append elements
-	inner.appendChild(content);
-	// Non-automatic message
-	if (!auto) {
-		// User message, create author section
-		if (Chat.lastMessageSender == authorName) {
-			// This is the same author who sent the last message
-			// Do not display the author element and reduce message padding
-			message.style.padding = "0 4px"
-		} else {
-			// Message from a new author
-			// Display an author element
-			let author = document.createElement("span");
-			author.className = "MessageAuthor";
-			author.textContent = authorName;
-			author.style.color = authorColor;
-			message.appendChild(author)
-		}
-		// Update last message author
-		// console.log("Message envoyé par " + authorName)
-		Chat.lastMessageSender = authorName;
-		let date = document.createElement("span");
-		date.className = "MessageDate";
-		date.textContent = "maintenant";
-		inner.appendChild(date)
-	}
-	message.appendChild(inner);
-	// Show message
-	MessageList.appendChild(message);
-	setMessageListPosition(listFullHeight, listVisibleHeight)
-},
-setMessageListPosition = (fullHeight, visibleHeight) => {
-	// Scroll to end of the message list
-	if (MessageList.scrollTop == (fullHeight - visibleHeight)) {
-		// The user is already at the end of the message list, continue scrolling
-		MessageList.scrollTop = MessageList.scrollHeight
-	}
-};
 
 // Send message event listener
 Form.sendMessage.addEventListener("submit", (e) => {
