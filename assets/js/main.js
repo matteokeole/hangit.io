fd9aadea64877f71d2ab8d6c7f922fec1872c727
Mattéo Legagneux <matteoo34@icloud.com>
2021-12-01 09:13:31 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,8 +25,8 @@ r.addEventListener("load", () => {
 		Player.role = "host";
 		invitationLink = GenerateLink();
 		current_url += `?g=${invitationLink}`;
-		// Input.invitationLink.value = `https://matteoo34.github.io/hangit.io/?g=${invitationLink}`;
-		Input.invitationLink.value = `http://localhost/hangit.io/?g=${invitationLink}`;
+		Input.invitationLink.value = `https://matteoo34.github.io/hangit.io/?g=${invitationLink}`;
+		// Input.invitationLink.value = `http://localhost/hangit.io/?g=${invitationLink}`;
 		// Input.invitationLink.value = `http://localhost:2021/?g=${invitationLink}`;
 		toggleDisplay(Container.nickname);
 		toggleDisplay(Container.openHostForm);
